7f24106d1e7e02d080ab66d5bb028b11b4bd350d
prolic <saschaprolic@googlemail.com>
2019-01-04 06:12:33 +0800

    add test helpers

---
+++ tests/DefaultData.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of `prooph/event-store-http-client`.
+ * (c) 2018-2019 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2019 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreHttpClient;
+
+use Prooph\EventStore\UserCredentials;
+
+class DefaultData
+{
+    public static function adminUsername(): string
+    {
+        return SystemUsers::ADMIN;
+    }
+
+    public static function adminPassword(): string
+    {
+        return SystemUsers::DEFAULT_ADMIN_PASSWORD;
+    }
+
+    public static function adminCredentials(): UserCredentials
+    {
+        return new UserCredentials(self::adminUsername(), self::adminPassword());
+    }
+}
---
+++ tests/SystemUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of `prooph/event-store-http-client`.
+ * (c) 2018-2019 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2019 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreHttpClient;
+
+class SystemUsers
+{
+    public const ADMIN = 'admin';
+    public const OPERATIONS = 'ops';
+    public const DEFAULT_ADMIN_PASSWORD = 'changeit';
+}
