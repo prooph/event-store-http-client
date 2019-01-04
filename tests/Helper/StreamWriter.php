ed97ba13967a000e1fd232d36314a93c6823f5bb
prolic <saschaprolic@googlemail.com>
2019-01-05 03:54:30 +0800

    add tests/appending_to_implicitly_created_stream

--- tests/Helper/StreamWriter.php
+++ tests/Helper/StreamWriter.php
@@ -41,15 +41,8 @@ class StreamWriter
             $expVer = $this->version === ExpectedVersion::ANY ? ExpectedVersion::ANY : $this->version + $key;
             $result = $this->connection->appendToStream($this->stream, $expVer, [$event]);
             \assert($result instanceof WriteResult);
-            $nextExpVer = $result->nextExpectedVersion();
-
-            if ($this->version !== ExpectedVersion::ANY
-                && ($expVer + 1) !== $nextExpVer
-            ) {
-                throw new RuntimeException('Wrong next expected version');
-            }
         }
 
-        new TailWriter($this->connection, $this->stream);
+        return new TailWriter($this->connection, $this->stream);
     }
 }
