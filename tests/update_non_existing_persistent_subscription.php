e7893aa1a73174d86f84b13d8dfda8e19d7538ae
prolic <saschaprolic@googlemail.com>
2019-01-06 06:03:08 +0800

    tests/update_non_existing_persistent_subscription

---
+++ tests/update_non_existing_persistent_subscription.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of `prooph/event-store-http-client`.
+ * (c) 2018-2019 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2019 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreHttpClient;
+
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStore\Exception\InvalidOperationException;
+use Prooph\EventStore\PersistentSubscriptionSettings;
+use Prooph\EventStore\Util\Guid;
+use ProophTest\EventStoreHttpClient\Helper\TestConnection;
+
+class update_non_existing_persistent_subscription extends TestCase
+{
+    /** @test */
+    public function the_completion_fails_with_not_found(): void
+    {
+        $conn = TestConnection::create();
+
+        $stream = Guid::generateAsHex();
+        $settings = PersistentSubscriptionSettings::create()
+            ->doNotResolveLinkTos()
+            ->startFromCurrent()
+            ->build();
+
+        $this->expectException(InvalidOperationException::class);
+
+        $conn->updatePersistentSubscription(
+            $stream,
+            'existing',
+            $settings,
+            DefaultData::adminCredentials()
+        );
+    }
+}
