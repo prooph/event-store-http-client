9da51f084c3806cef6187c0c188c59f55d5e591c
prolic <saschaprolic@googlemail.com>
2019-01-06 06:00:04 +0800

    tests/create_persistent_subscription_with_too_big_checkpoint_after

---
+++ tests/create_persistent_subscription_with_too_big_checkpoint_after.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of `prooph/event-store-http-client`.
+ * (c) 2018-2019 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2019 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStoreHttpClient;
+
+use PHPUnit\Framework\TestCase;
+use Prooph\EventStore\Exception\InvalidArgumentException;
+use Prooph\EventStore\PersistentSubscriptionSettings;
+
+class create_persistent_subscription_with_too_big_checkpoint_after extends TestCase
+{
+    /** @test */
+    public function the_build_fails_with_argument_exception(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        PersistentSubscriptionSettings::create()->checkPointAfterMilliseconds(25 * 365 * 24 * 60 * 60 * 1000)->build();
+    }
+}
